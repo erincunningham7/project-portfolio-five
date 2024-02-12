c84e620f62393006e12a351904792f8e796baca1
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-12 01:19:11 +0000

    customise navbar.html

--- cleancosmetics/settings.py
+++ cleancosmetics/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-erincunning-projectport-5owj50gunip.ws-eu108.gitpod.io','.herokuapp.com']
 
