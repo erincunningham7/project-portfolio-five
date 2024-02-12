4ebf5c763ace79bbc33dc45fd2a34f6ca8fc3486
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-12 20:39:21 +0000

    customize admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Category, Brand, Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'brand', 'category', 'price', 'created_on']
+    list_filter = ['brand', 'category', 'created_on']
+    search_fields = ['title', 'brand__name', ]
+
+    ordering = ['-created_on']
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+
 admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductAdmin)
