17c86d8c3de6d5e51843b88666120836fc5e05a1
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-13 12:30:26 +0000

    add category filtering

--- products/models.py
+++ products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=150)
-
+    friendly_name = models.CharField(max_length=150, null=True, blank=True)
+    
     class Meta:
         verbose_name_plural = 'Categories'
 
--- products/views.py
+++ products/views.py
@@ -10,6 +10,14 @@ def all_products(request):
     '''
     products = Product.objects.all().order_by('-created_on')
     query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # print(categories)
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,6 +31,8 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
