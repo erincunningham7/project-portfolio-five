6b2346c5ea227cf8be650f23b901cd5588265480
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-14 23:31:18 +0000

    update toast functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -12,18 +12,18 @@ def add_to_bag(request, item_id):
     """
     A view that handles adding items to cart
     """
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        # messages.success(request, f'{product.title} added to your cart')
+        messages.success(request, f'{product.title} added to your cart')
         # print(messages.success)
     else:
         bag[item_id] = quantity
-        messages.error(request, f'{product.title} added to your cart')
+        messages.success(request, f'{product.title} added to your cart')
 
     request.session['bag'] = bag
     # print(request.session['bag'])
