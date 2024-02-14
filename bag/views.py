434cfbd3db36bbb72e9f3976afaa8f4d4cf08c6a
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-14 00:55:23 +0000

    update toast classes and add test message

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 def shopping_bag(request):
@@ -10,12 +12,14 @@ def add_to_bag(request, item_id):
     """
     A view that handles adding items to cart
     """
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'{product.title} added to your cart')
     else:
         bag[item_id] = quantity
 
