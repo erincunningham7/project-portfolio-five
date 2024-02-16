ae44243bcc75b3da824d07bd83096afc3fc91734
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-16 17:43:45 +0000

    create checkout app and models

--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True, default='images/noimage.png')
     created_on = models.DateTimeField(auto_now_add=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     # in_stock = models.BooleanField(default=True)
     # stock_amount = models.IntegerField(default=1)
     # on_sale = models.BooleanField(default=False)
