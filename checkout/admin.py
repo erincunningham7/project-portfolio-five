088a7de7187f46901a03672a2bd74a1fab3b0d32
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-17 01:46:09 +0000

    add admin, forms and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('date','order_number', 'order_total', 'delivery_cost', 'grand_total',)
+
+    field = ('date', 'order_number', 'full_name', 'phone_number', 'email', 'street_address1', 'street_address2', 'town_or_city', 'county', 'country', 'postcode', 'town_or_city', 'delivery_cost', 'order_total', 'grand_total',)
+
+    list_display = ('date', 'order_number','full_name', 'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('date',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'phone_number', 'email', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'country', 'county',)
