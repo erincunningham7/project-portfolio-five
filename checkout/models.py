073c6a1837bfe760c4e6e22e71799ef1e03c4138
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-19 23:23:17 +0000

    add profile models, views, urls and template

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 from django.db.models import Sum
 from products.models import Product
+from profiles.models import UserProfile
 
 # Create your models here.
 STATUS = (
@@ -14,6 +15,7 @@ STATUS = (
 
 class Order(models.Model):
     order_number = models.CharField(max_length=40, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=60, null=False, blank=False)
     email = models.EmailField(max_length=150, null=False, blank=False)
     phone_number = models.CharField(max_length=10, null=False, blank=False)
