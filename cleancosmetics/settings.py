ffd38783da60d79543cca6fbdb8c831a460ec1a9
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-21 21:06:17 +0000

    add contact link to navbar

--- cleancosmetics/settings.py
+++ cleancosmetics/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-erincunning-projectport-b8w42e8yo8h.ws-eu108.gitpod.io','.herokuapp.com']
 
