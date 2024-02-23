69a8554d20efd11c6d392772d8f8bfe4525433c1
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-23 16:12:07 +0000

    display blog button in navbar and blog page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.TopicList.as_view(), name='topic'),
+    path('', views.Topics.as_view(), name='topics'),
     path('<slug:slug>/', views.TopicDetail.as_view(), name='topic_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,13 @@ from django.views import generic
 from .models import Topic
 
 # Create your views here.
-class TopicList(generic.ListView):
+class Topics(generic.ListView):
     queryset = Topic.objects.filter(status=1).order_by('-created_on')
-    template_name = 'topics.html'
+    template_name = 'blog/topics.html'
+    
+    def topic_display(request):
+
+        return render(request, 'topics.html')
 
 class TopicDetail(generic.DetailView):
     model = Topic
