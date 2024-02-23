db6fbae148345ad140d51625b7da0eebc3605926
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-23 13:14:51 +0000

    remove comments and update review model

--- reviews/models.py
+++ reviews/models.py
@@ -14,7 +14,7 @@ RATING = (
 class UserReview(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING, default=None)
+    rating = models.IntegerField(choices=RATING, default='', null=True)
     created = models.DateTimeField(auto_now_add=True)
     review = models.TextField()
 
