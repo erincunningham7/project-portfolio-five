a0a73a6b7112b200a71f0de68bff298168998936
erincunningham7 <cunninnghamerin@gmail.com>
2024-02-23 21:09:35 +0000

    get tredning topics page running successfully

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,10 @@ from django.views import generic
 from .models import Topic
 
 # Create your views here.
-class Topics(generic.ListView):
+class Topic(generic.ListView):
     queryset = Topic.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/topics.html'
-    
-    def topic_display(request):
-
-        return render(request, 'topics.html')
 
 class TopicDetail(generic.DetailView):
     model = Topic
-    template_name = 'topic_detail.html'
+    template_name = 'blog/topic_detail.html'
