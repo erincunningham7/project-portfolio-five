f4150661403d33443695fc54d3385769a2054923
erincunningham7 <cunninnghamerin@gmail.com>
2024-06-10 14:55:48 +0000

    alter contact model, view and template for saving successfully to tthe database

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Contact(models.Model):
-    full_name = models.CharField()
+    full_name = models.CharField(max_length=250)
     email = models.EmailField()
-    subject = models.CharField()
-    message = models.CharField()
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=3000)
--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, reverse
 from .forms import ContactUs
 from django.http import HttpResponse
-from django.core.mail import send_mail
 from django.views.generic import FormView, TemplateView
+from django.urls import reverse_lazy
 from django.conf import settings
 
 
@@ -22,28 +22,11 @@ class ContactView(FormView):
 
     form_class = ContactUs
     template_name = "contact/contact.html"
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('success')
 
-    def get_success_url(self):
-        return reverse("contact")
-
-    def contact(self, form):
-        """
-        Clean each field, layout the format of the message,
-        use send_email to send the subject, message,
-        from email, and recipient list
-        """
-        full_name = form.cleaned_data.get("full_name")
-        email = form.cleaned_data.get("email")
-        subject = form.cleaned_data.get("subject")
-        message = form.cleaned_data.get("message")
-
-        contact_message = (
-            f"New contact message from {full_name} at {email}, {subject}: {message}"
-        )
-        send_mail(
-            subject="New contact form submission",
-            message=full_message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[settings.NOTIFY_EMAIL],
-        )
+    def form_valid(self, form):
+        user = form.save()
+        
         return super(ContactView, self).form_valid(form)
+
