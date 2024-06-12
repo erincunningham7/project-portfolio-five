ffe42c92cf4531259f8905365f197ac7a76c2c44
erincunningham7 <cunninnghamerin@gmail.com>
2024-06-12 19:08:43 +0000

    modify checkout and bag files and settings.py to ensure delivery cost updates successfully

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,8 +12,8 @@ def bag_contents(request):
     bag_content = []
     total = 0
     product_count = 0
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    grand_total = delivery + total
+    delivery_cost = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    grand_total = delivery_cost + total
     bag = request.session.get("bag", {})
 
     for item_id, quantity in bag.items():
@@ -27,14 +27,23 @@ def bag_contents(request):
                 "product": product,
             }
         )
+    
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery_cost = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery_cost = 0
+        free_delivery_delta = 0
 
-    grand_total = delivery + total
+    grand_total = delivery_cost + total
 
     context = {
         "bag_content": bag_content,
         "total": total,
         "product_count": product_count,
-        "delivery": delivery,
+        "delivery_cost": delivery_cost,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         "grand_total": grand_total,
     }
 
