86d177ceee6889ebd94c71dc155714224c84891f
erincunningham7 <cunninnghamerin@gmail.com>
2024-07-08 15:12:27 +0000

    change static directory structure and checkout.html hyperlinks accordingly

--- cleancosmetics/settings.py
+++ cleancosmetics/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-erincunning-projectport-fbv3d2mmbww.ws-eu115.gitpod.io",
